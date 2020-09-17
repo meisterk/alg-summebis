a0e285e1468df0407a9cfa929918959946b80863
meisterk <franz.kohnle@sabel.com>
2020-09-17 14:43:16 +0200

    Write tests

---
+++ summebis.py
@@ -0,0 +1,26 @@
+import unittest
+
+# Function summe_bis:
+# Parameter: n
+# Result: 1 + 2 + 3 + ... + n
+
+
+def summe_bis(number):
+    pass
+
+
+# Testcases
+class TestSummeBis(unittest.TestCase):
+    def test_summe_bis_1_is_1(self):
+        self.assertEqual(summe_bis(1), 1)
+
+    def test_summe_bis_2_is_3(self):
+        self.assertEqual(summe_bis(2), 3)
+
+    def test_summe_bis_10_is_55(self):
+        self.assertEqual(summe_bis(10), 55)
+
+
+# Run Tests
+if __name__ == "__main__":
+    unittest.main()
